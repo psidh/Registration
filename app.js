9e0fa3a75fd0ecde1b4c2054838bcbc6902553a1
P SIDHARTH <philkhanasidharth14@gmail.com>
2024-01-13 22:46:02 +0530

    initial commit

--- app.js
+++ app.js
@@ -3,6 +3,17 @@ function generateQRCode() {
   const branch = document.getElementById('branch').value;
   const college = document.getElementById('college').value;
 
+  // Update URL with parameters
+  const params = new URLSearchParams({
+      name: name,
+      branch: branch,
+      college: college,
+  });
+
+  const url = window.location.href.split('?')[0] + '?' + params.toString();
+  window.history.replaceState({}, '', url);
+
+  // Generate QR Code
   const data = `Name: ${name}\nBranch: ${branch}\nCollege: ${college}`;
   const qrCodeDiv = document.getElementById('qr-code');
   qrCodeDiv.innerHTML = '';
@@ -12,4 +23,5 @@ function generateQRCode() {
       width: 128,
       height: 128
   });
+  
 }
