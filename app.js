df34b71b06bcbaae091e435e563668cb39507d58
Philkhana Sidharth <philkhanasidharth14@gmail.com>
2024-01-15 18:52:28 +0530

    added the correctLevel data

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ function generateQRCode() {
     new QRCode(qrCodeDiv, {
         text: name,
         width: 128,
-        height: 128
+        height: 128,
+        // new thing added
+        correctLevel : QRCode.CorrectLevel.L
     });
 }
 window.onload = function() {
